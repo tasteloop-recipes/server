c8ff924d76cee990c0ea267b767b0f6e46e50875
Luis Augusto <hello@luis.app>
2025-11-01 11:17:13 -0700

    add stricter type checking

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap().catch((err) => {
+bootstrap().catch((err: unknown) => {
   console.error('Error during application bootstrap:', err);
   process.exit(1);
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,7 +6,7 @@ import type { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication<App> | undefined = undefined;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
