9581a051758d905fdbc54822caa68c56e9c8175e
Luis Augusto <hello@luis.app>
2025-11-02 16:19:11 -0800

    fix migrations and e2e tests

--- prisma/migrations/20241206_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateExtension
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
--- CreateTable
-CREATE TABLE IF NOT EXISTS "User" (
-  "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-  "name" TEXT NOT NULL,
-  CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
