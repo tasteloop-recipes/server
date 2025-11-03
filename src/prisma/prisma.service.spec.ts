006706185f7577b31f6e5a1ce7d3da34c9793927
Luis Augusto <hello@luis.app>
2025-11-02 16:30:26 -0800

    remove naming convention rul

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -2,7 +2,6 @@ const connectMock = jest.fn();
 const disconnectMock = jest.fn();
 
 jest.mock('@prisma/client', () => ({
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   PrismaClient: class {
     $connect = connectMock;
     $disconnect = disconnectMock;
