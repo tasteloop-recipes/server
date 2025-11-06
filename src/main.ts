1c5aef744d99b194f586f7d19af61ca2318a8e43
Luis Augusto <hello@luis.app>
2025-11-06 12:38:15 -0800

    fix validators and add test coverage

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   // Configure CORS for public endpoints
   app.enableCors({
